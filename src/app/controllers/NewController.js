b6c04e73b1863221d7eaf3e9bcd231e159de7a9b
StarEdit <phuongtan9920@gmail.com>
2021-07-26 10:42:26 +0700

    26//7

---
+++ src/app/controllers/NewController.js
@@ -0,0 +1,13 @@
+class NewsController {
+    // [GET] /News
+    index(req, res) {
+        res.render('news');
+    }
+
+    // [GET] /News/:slug
+    show(req, res) {
+        res.send('news detail');
+    }
+}
+
+module.exports = new NewsController;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
+
+const route = require('./routes');
+
 const app = express();
 const port = 3000;
 
@@ -20,17 +23,8 @@ app.engine(
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
-
-app.get('/new', (req, res) => {
-    res.render('new');
-});
-
-app.get('/search', (req, res) => {
-    res.render('search');
-});
+// Routes init
+route(app);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
